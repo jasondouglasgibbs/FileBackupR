95307342993336e630c52702755e2e3d4b7975e1
Jason G <jasondouglasgibbs@gmail.com>
2022-04-15 10:47:36 -0400

    -Initial code commit.

---
+++ FileBackupR.R
@@ -0,0 +1,73 @@
+##Script to back up files to another drive.##
+##Author: Jason D. Gibbs##
+
+##Libraries
+library(tidyverse)
+library(readxl)
+
+##Inputs##
+DestinationFolder<-"D:/Gibbs"
+
+##Read in list of folders to transfer##
+FoldersToTransfer<-as.data.frame(read_xlsx("LocalFileList.xlsx"))
+
+
+##Transfers files that don't exist on the backup drive.
+for(i in 1:nrow(FoldersToTransfer)){
+file.copy(from=FoldersToTransfer[i, "Path"], to=DestinationFolder, recursive = TRUE, overwrite = FALSE)
+}
+
+
+for (i in 1:nrow(FoldersToTransfer)){
+  if(i==1){
+  FileList<-as.data.frame(list.files(FoldersToTransfer[i,"Path"], full.names = TRUE, recursive = TRUE))
+  }else{
+    NextList<-as.data.frame(list.files(FoldersToTransfer[i,"Path"], full.names = TRUE, recursive = TRUE))
+    FileList<-rbind(FileList,NextList)
+  }
+
+}
+colnames(FileList) <- c("File_Path")
+
+##Creates a column for modified time.##
+FileList$Modified_Time<-file.mtime(FileList$File_Path)
+FileList$Reduced_Path<-paste0(basename(dirname(FileList[,"File_Path"])),"/", basename(FileList[,"File_Path"]))
+
+##Destination Folder
+DestinationFiles<-as.data.frame(list.files(DestinationFolder, full.names=TRUE, recursive = TRUE))
+colnames(DestinationFiles) <- c("File_Path")
+DestinationFiles$Modified_Time<-file.mtime(DestinationFiles$File_Path)
+DestinationFiles$Reduced_Path<-paste0(basename(dirname(DestinationFiles[,"File_Path"])),"/", basename(DestinationFiles[,"File_Path"]))
+
+
+##Finds files that exist both in the main folder and the destination folder.
+##Removes the files if they exist in both locations.##
+RemovalList<-list()
+
+for(i in 1:nrow(FileList)){
+  for(j in 1:nrow(FileList)){
+    if(FileList[i,"Reduced_Path"]==DestinationFiles[j, "Reduced_Path"]&FileList[i, "Modified_Time"]<DestinationFiles[j,"Modified_Time"]){
+      RemovalListAdd<-i
+      RemovalList<-rbind(RemovalList,RemovalListAdd)
+    }
+  }
+}
+
+rownames(FileList)<-NULL
+RemovalList<-sapply(RemovalList,"[[",1)
+UpdatedFiles<-FileList[-RemovalList,]
+
+if(nrow(UpdatedFiles>0)){
+  UpdatedFiles$Destination_Path<-NA
+  for(i in 1:nrow(UpdatedFiles)){
+    FilterValue<-UpdatedFiles[i, "Reduced_Path"]
+    DestinationFilter<-filter(DestinationFiles, Reduced_Path==FilterValue)
+    UpdatedFiles[i,"Destination_Path"]<-DestinationFilter[1,"File_Path"]
+    file.copy(from=UpdatedFiles[i, "File_Path"], to=UpdatedFiles[i,"Destination_Path"], recursive = FALSE, overwrite = TRUE)
+  }
+  print(paste0(i," modified file(s) updated."))
+}else{
+  print("No modified files found.")
+  
+}
+
